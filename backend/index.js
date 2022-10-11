5aa9624e26b059aceba41a0724d8c90307877425
Noman Ejaz <nomanejaz132@gmail.com>
2022-10-11 13:50:45 +0500

    removed unused code

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const http = require('http');
 const fs = require('fs');
 
 const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
-const productsData = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const headers = {
